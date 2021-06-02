ffe53e03493589fbd6745c113e4862bbf01770e0
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-02 14:28:05 -0700

    WIP: fix output fields

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,44 @@ function divide(number1, number2) {
   return number1 / number2;
 };
 
+//User Interface Logic
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $('#output').text(result);
+    $('#outputAddition').text(result);
   });
 });
+
+$(document).ready(function() {
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#subtract1").val());
+		const number2 = parseInt($("#subtract2").val());
+		const result = subtract(number1, number2);
+		$('#outputSubtraction').text(result);
+	})
+})
+
+$(document).ready(function() {
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#multiply1").val());
+		const number2 = parseInt($("#multiply2").val());
+		const result = subtract(number1, number2);
+		$('#outputMultiplication').text(result);
+	})
+})
+
+$(document).ready(function() {
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#divide1").val());
+		const number2 = parseInt($("#divide2").val());
+		const result = subtract(number1, number2);
+		$('#outputDivision').text(result);
+	})
+})
